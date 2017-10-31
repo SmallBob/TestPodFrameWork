2545a1d1c2d74a565dd39cd0abba0ba34ec069bc
初末 <wk132544@alibaba-inc.com>
2017-10-31 20:37:49 +0800

    4

--- TestPodFrameWork/TestPodFrameWork/TestPodFrameWork.h
+++ TestPodFrameWork/TestPodFrameWork/TestPodFrameWork.h
@@ -5,7 +5,7 @@
 //  Created by 王凯 on 2017/10/31.
 //  Copyright © 2017年 com.alibaba.ALPlayerVodSDK. All rights reserved.
 //
-
+#import <UIKit/UIKit.h>
 //! Project version number for TestPodFrameWork.
 FOUNDATION_EXPORT double TestPodFrameWorkVersionNumber;
 
